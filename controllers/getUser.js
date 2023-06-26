557984e406bf7a2b1e2602783333d239b846b270
Nevish-302 <shiven302@gmail.com>
2023-06-27 00:31:25 +0530

    Hello\panaaadf

--- controllers/getUser.js
+++ controllers/getUser.js
@@ -3,6 +3,27 @@ const User = require('../models/user.model')
 const jwt = require('jsonwebtoken')
 const Fetch = require('node-fetch')
 
+const get_items = async (AccessToken) => {
+    try {
+    const jack = await Fetch(`https://api.spotify.com/v1/me/player/currently-playing`, {
+      method: 'GET',                               // Replace with the HTTP method you want to use
+      headers: {
+      'Authorization': `Bearer ${AccessToken}`,    // Include the bearer token in the Authorization header
+      'Scope' : 'user-read-currently-playing'
+    }
+  }).then(response => {if(response.status == 200) {return response.json();} else {console.log(response.status) ;return null}})
+  
+  console.log(jack, 'jack')
+  return {name : jack.item.name, img : jack.item.album.images[0].url}
+  
+}
+  catch (err) {
+    console.log(err)
+    return err
+}
+  }
+  
+
 const getStory = async (AccessToken) => {
     const jack = await Fetch(`https://api.spotify.com/v1/me/player/currently-playing`, {
     method: 'GET',                               // Replace with the HTTP method you want to use
@@ -23,14 +44,14 @@ return jack;
 const getUser = async (req, res) => {
     const {user_id} = await jwt.verify(req.headers.authorization.split(" ")[1], process.env.TOKEN_KEY);
     const {AccessToken} = await req.body;
-    const Story = await getStory(AccessToken)
+    const Story = await get_items(AccessToken)
     User.findOne({_id : user_id}).then((data) => {
         if (!data)  {
             console.log("User not found");
             res.status(400).json({msg : "User not found"})
         }
         //console.log("Hello")
-        console.log(data, user_id, "hi")
+        //console.log(data, user_id, "hi")
 
         res.status(200).json({response : data, story : Story})
         
