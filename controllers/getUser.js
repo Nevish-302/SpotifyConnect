70d16109ed1361875a693316bcc9d9148fdd2855
Nevish-302 <shiven302@gmail.com>
2023-06-26 21:02:05 +0530

    Hello\panaa

--- controllers/getUser.js
+++ controllers/getUser.js
@@ -10,8 +10,11 @@ const getStory = async (AccessToken) => {
     'Authorization': `Bearer ${AccessToken}`,    // Include the bearer token in the Authorization header
     'Scope' : 'user-read-currently-playing'
   }
-}).then(j=> j.json()) 
-return {name : jack.item.name, url : jack.item.images[0].url}
+}).then(j=> j.json()).then(res => {
+    print(res)
+return {name : res.item.name, url : res.item.images[0].url}}
+)
+return jack;
 }
 
 const getUser = async (req, res) => {
