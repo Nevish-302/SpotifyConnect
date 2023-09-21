cef0c17b2e2bafd3b8cb06b0c0637debf8b6b8e1
Shiven <Shiven302@gmail.com>
2023-09-21 12:27:44 +0530

    Update app.js
    Chat Features

--- app.js
+++ app.js
@@ -41,3 +41,65 @@ app.use('/chats', chats);
 app.listen(5000, () => {
   console.log(`The server is listening on the port 5000`);
 });
+//Socket Code
+
+//var http = require("http").Server(app);
+//var io = require("socket.io")(http);
+const chats = require('./models/chat.model')
+const User = require('./models/user.model')
+)
+const io = require("socket.io")(server, {
+  cors : {
+    //origin : ["http://localhost:8080", "https://admin.socket.io/"],
+    origin : "*",
+  }
+})
+module.exports=io
+
+const socketIo = io.of('/socket') 
+
+socketIo.on('connection', socket=>{
+  socket.on('ping', n =>console.log(n))
+  socket.on('custom-event', (a, b, c) => {
+      console.log(a, b, c);
+  })
+//Team Getting Resource
+socket.on('send-chat-message', async (request, cb) =>{
+  
+  if(request.senderId && request.receiverId){
+  const sender = await User.find({_id : request.senderId})
+  const receiver = await User.find({_id : request.receiverId})
+  if(!sender || !receiver)
+  {
+    cb(
+      {
+        status: "failure",
+        data: {
+            message : "Incorrect Ids" ,
+        }
+    }
+    )
+  }
+else {
+    const chats = await Chat.findOneAndUpdate({userIdOne : sender._id, userIdTwo : sender._id}, {$push: {messages : {message : request.message.content, sentBy : request.message.sentBy})
+    socket.broadcast.emit('receive-chat-message', request.receiverId)
+    cb(
+    {
+      status: "success",
+      data: {
+          resource : chats ,
+      }
+  })
+}
+}
+  else
+  {
+    cb({
+      status: "failure",
+      data: {
+          message : "Provide the ids",
+      }
+  })
+  }
+})
+})
